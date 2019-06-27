475cd704b044b4fe23b61a9f2b63d41de720a6bc
beytullahatik <51950050+beytullahatik@users.noreply.github.com>
2019-06-27 13:06:30 +0300

    Add files via upload

---
+++ Java_SOAPUI_Calculator/CalculatorwithSOAPUI.java
@@ -0,0 +1,22 @@
+package controller;
+
+public class CalculatorDemo {
+	private static double add(double NumberA, double NumberB)
+	{
+		return NumberA+NumberB;
+	}
+	private static double sub(double NumberA, double NumberB)
+	{
+		return NumberA-NumberB;
+	}
+	private static double mul(double NumberA, double NumberB)
+	{
+		return NumberA*NumberB;
+	}
+	private static double div(double NumberA, double NumberB)
+	{
+		return NumberA/NumberB;
+	}
+	
+
+}
