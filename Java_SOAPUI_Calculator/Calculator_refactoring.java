ed151e5f3fc239678736c5d9745c7861446512f6
beytullahatik <51950050+beytullahatik@users.noreply.github.com>
2019-06-27 15:29:12 +0300

    Add files via upload

---
+++ Java_SOAPUI_Calculator/Calculator_refactoring.java
@@ -0,0 +1,26 @@
+package controller;
+
+public class Demo2 {
+	
+	
+	
+	public static double Add (double firstNumber, double secondNumber)
+		{
+			return  firstNumber + secondNumber;
+		}
+	public static double Substract (double  firstNumber, double secondNumber)
+		{
+			return  firstNumber - secondNumber;
+		}
+	public static double Multiply (double  firstNumber, double secondNumber)
+		{
+			return  firstNumber * secondNumber;
+		}
+	public static double Divide (double  firstNumber, double secondNumber)
+		{
+			return  firstNumber / secondNumber;
+		}
+
+	
+
+}
